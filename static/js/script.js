e9dec63017421cf91a4e6b04e46e3455ca4c7512
Oğuzhan KORKMAZ <oguzhannkrkmz@users.noreply.github.com>
2020-06-02 00:34:39 +0300

    commit kotkafali

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+//     $(".box1").hover(function () {
+//         $(".bg, .box1").css("background", "linear-gradient(135deg, #e2a9e5 15%,#2b94e5 100%)").css("background", "-moz-linear-gradient(-45deg, #e2a9e5 15%, #2b94e5 100%)").css("background", "-webkit-linear-gradient(-45deg, #e2a9e5 15%,#2b94e5 100%)").css("opacity", "1")
+//     }, function () {
+//         $(".bg, .box1").css("background-color", "none").css("opacity", "0")
+//     })
+// })
+
+// $(document).ready(function () {
+//     $(".box2").hover(function () {
+//         $(".bg, .box2").css("background", "linear-gradient(135deg, #e2a9e5 15%,#2b94e5 100%)").css("background", "-moz-linear-gradient(-45deg, #e2a9e5 15%, #2b94e5 100%)").css("background", "-webkit-linear-gradient(-45deg, #e2a9e5 15%,#2b94e5 100%)").css("opacity", "1")
+//     }, function () {
+//         $(".bg, .box2").css("background-color", "none").css("opacity", "0")
+//     })
+// })
